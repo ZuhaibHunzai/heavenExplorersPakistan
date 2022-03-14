d15121133a625e05ba8c66052d0c905c379fde82
ZuhaibHunzai <80482362+ZuhaibHunzai@users.noreply.github.com>
2022-03-14 18:56:40 +0500

    checking status

--- src/Components/Hero/index.js
+++ src/Components/Hero/index.js
@@ -15,7 +15,7 @@ const Hero = () => {
       <div className={classes.HeroTextArea}>
         <div className={classes.heroHeading}>
           <Typography variant="h2" className={classes.heroMainH}>
-            Heaven Explorers Pakistan
+            Nouman Aka Barataling
           </Typography>
         </div>
         <div className={classes.heroPara}>
